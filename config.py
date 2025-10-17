215790cb084df64f794e7bb0606e678dbb122f20
Antony Gitee <antonygitee@gmail.com>
2025-10-17 20:05:42 +0300

    Update config to use Clever Cloud MySQL

--- config.py
+++ config.py
@@ -5,13 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # MySQL database configuration
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_NAME = os.getenv("DB_NAME")
-
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_ADDON_URI", "sqlite:///chicken_manager.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey") 
